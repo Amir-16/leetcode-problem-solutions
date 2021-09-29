9c072f8f7c73a3d8d7ab6a909f87b0165ffe902a
Your Name <greenfitter516@gmail.com>
2021-09-30 00:50:48 +0600

    two-sum

---
+++ Two-sum/twoSum.php
@@ -0,0 +1,29 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $extra_array = array();
+        
+        for ($i = 0;$i < count($nums);$i++) {
+            $extra_array[$target - $nums[$i]] = $i;
+        }
+        
+         var_dump($extra_array);
+        
+        for ($j = 0;$j < count($nums);$j++) {
+            if (array_key_exists($nums[$j], $extra_array) && $j !== $extra_array[$nums[$j]]) {
+                return array($j, $extra_array[$nums[$j]]);
+            }
+        }
+        
+        
+        
+        return array(0,0);
+    }
+}
