5a06e23c4b460d3f838ce658e5bbfd303fe23d00
Amir-16 <greenfitter516@gmail.com>
2022-03-28 14:41:28 +0600

    wr

--- 1491. Average Salary/average-salary.php
+++ 1491. Average Salary/average-salary.php
@@ -5,24 +5,30 @@ class Solution{
 
     public $mx;
     public $mn;
+    public $n ;
     function average(array $salary){
          $sum =0;
-        for($i=0;$i<sizeof($salary);$i++){
+        $n =sizeof($salary);
+
+        for($i=0;$i<$n;$i++){
            // array_push($salary,$i);
            $sum = $sum + $salary[$i];
-            $value =$salary[$i];
+            $value[] =$salary[$i];
            $mx =max($value);
+           $mn = min($value);
          
        }
-      // echo $sum;
-       echo $mx;
+     // echo $sum;
+
+      $result = $sum -($mx + $mn) /(($n-2)*1.0);
+
+      echo $result;
 
            // $sum =array_sum($salary);
         
     }
 
 }
-
  
 $ans = new Solution();
 $ans->average([10,20,40,50,70]);
