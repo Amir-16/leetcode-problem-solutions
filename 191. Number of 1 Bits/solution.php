96bc61d4231de130b67aedc4d2d986fa377818c6
Amir-16 <greenfitter516@gmail.com>
2022-06-12 18:35:08 +0600

    add

--- 191. Number of 1 Bits/solution.php
+++ 191. Number of 1 Bits/solution.php
@@ -3,14 +3,33 @@
 
 class Solution
 {
-    /**
-     * @param Integer $n
-     * @return Integer
-     */
-    function hammingWeight($n)
+ 
+
+    function hammingWeightint(int $n)
     {
+
+          $pivot =1;
+         $sum =0;
+    
+        for($i =0; $i<32;$i++){
+
+             $check = $pivot << $i;
+
+             if(($n & $check) == $check){
+                 $sum++;
+             }
+
+             echo $sum;
+             
+        }
+
     }
 }
+
+
+$solution = new Solution();
+
+$solution->hammingWeightint(00000000000000000000000000001011);
 
 
 
