e23ede352a971c53b22f92edefdebb6018556611
Amirul Islam <greenfitter516@gmail.com>
2022-06-13 08:45:41 +0600

    1779 start

---
+++ 1779. Find Nearest Point/solution.php
@@ -0,0 +1,20 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Integer $x
+     * @param Integer $y
+     * @param Integer[][] $points
+     * @return Integer
+     */
+    function nearestValidPoint($x, $y, $points)
+    {
+    }
+}
+
+
+
+
+?>
