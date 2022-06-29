dd62d13d7ed70d75d03a9d7fc7fd397b939f4c86
Amirul Islam <greenfitter516@gmail.com>
2022-06-29 09:16:40 +0600

    add

--- 1779. Find Nearest Point/solution.php
+++ 1779. Find Nearest Point/solution.php
@@ -11,12 +11,36 @@ class Solution
      */
     function nearestValidPointiny(int $x, int $y,  array $points)
     {
-        
+         $index = -1;
+
+         $distance =PHP_INT_MAX;
+
+         $n =sizeof($points);
+
+         for($i=0; $i<$n; $i++ ){
+
+            if($x == $points[$i][0] || $y ==$points[$i][1]){
+
+                $current_dis =abs( $x - $points[$i][0]) + abs($y - $points[$i][1]);
+
+                if($current_dis < $distance){
+                    $distance = $current_dis;
+                    $index = $i;
+                }
+
+            }
+            
+         }
+
+         echo $index;
     }
 
 }
 
 
 
+$solution = new Solution();
+
+$solution->nearestValidPointiny(3, 4, [[2, 4]]);
 
 ?>
