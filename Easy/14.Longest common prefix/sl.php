283382e11cee05687cba62da28bd0f03fecf8b60
Amirul Islam <greenfitter516@gmail.com>
2022-08-25 09:16:31 +0600

    14

---
+++ Easy/14.Longest common prefix/sl.php
@@ -0,0 +1,39 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param String[] $strs
+     * @return String
+     */
+    function longestCommonPrefix($strs)
+    {
+
+        $checkarray = array_shift($strs);
+        $str_match = str_split($checkarray);
+
+        $length = count($str_match);
+
+        $prefix = '';
+
+        for($i =0; $i< $length; $i++){
+
+            foreach($strs as $str){
+                if(!isset($str[$i])){
+                    break 2;
+                }
+                if($str)
+            }
+            
+        }
+
+
+        print_r($str_match);
+    }
+}
+
+
+$solution = new Solution;
+
+$solution->longestCommonPrefix(["flower", "flow", "flight"]);
--- Easy/14.Longest common prefix/solution.php
+++ Easy/14.Longest common prefix/solution.php
@@ -10,20 +10,22 @@ class Solution{
     function longestCommonPrefix($strs)
     {
         $first = array_shift($strs);
-        $first = str_split($first);
-        $length = count($first);
+        $check_match = str_split($first);
+        $length = count($check_match);
+
+       // print_r($length);
         $prefix = '';
         for ($i = 0; $i <= $length; $i++) {
             foreach ($strs as $str) {
                 if (!isset($str[$i])) {
                     break 2;
                 }
-                if ($str[$i] != $first[$i]) {
+                if ($str[$i] != $check_match[$i]) {
                     break 2;
                 }
             }
 
-            $prefix .= $first[$i];
+            $prefix .= $check_match[$i];
         }
         echo $prefix;
     }
