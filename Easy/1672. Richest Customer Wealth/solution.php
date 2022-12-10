e639efd655d27228126fc3fa691bdd4e8bfcafda
Amir-16 <greenfitter516@gmail.com>
2022-12-10 23:56:55 +0600

    add

---
+++ Easy/1672. Richest Customer Wealth/solution.php
@@ -0,0 +1,15 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[][] $accounts
+     * @return Integer
+     */
+    
+    function maximumWealth(array $accounts) {
+        
+        $value =PHP_INT_MIN;
+        $sum =0;
+    }
+}
