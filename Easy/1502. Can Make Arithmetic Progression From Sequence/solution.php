844a3100227a5e8519a229dcd116d5dd0b273742
Amir-16 <amirul.islam.riseuplabs@gmail.com>
2022-12-18 18:03:06 +0600

    add

--- Easy/1502. Can Make Arithmetic Progression From Sequence/solution.php
+++ Easy/1502. Can Make Arithmetic Progression From Sequence/solution.php
@@ -3,10 +3,33 @@
 
 class Solution {
 
+   // public $arr;
+
     function canMakeArithmeticProgression(array $arr) {
+
+     
+        $sortData= sort($arr);
+
+        $difference = $arr[1] - $arr[0];
+
+        $length = count($arr);
+
+        for($i=2; $i< $length;$i--){
+
+            if($arr[$i]-$arr[$i-1] != $difference){
+
+                return false;
+            }
+        }  
+        
+        return true;
         
     }
 }
+
+
+$solution =new Solution();
+$solution->canMakeArithmeticProgression([5,3,1]);
 
 
 
