287f23a070b10b814b71cb59977bb686bf38831c
Amir-16 <amirul.islam.riseuplabs@gmail.com>
2023-01-02 15:56:04 +0600

    520

---
+++ Easy/520. Detect Capital/solution.php
@@ -0,0 +1,44 @@
+<?php
+
+class Solution {
+
+    
+    //  @param String $word
+    //  @return Boolean
+    function detectCapitalUse($word) {
+
+        $value = strlen($word);
+        //echo $value;
+        
+        if($value ==1){
+            return true;
+        }else if($value >1){
+
+            if(ctype_upper($word)){
+                return true ;
+            }else{
+    
+                return false;
+            }
+        }
+        else{
+            for ($i = 1; $i < $value; $i++){
+                if($word[$i]== $word[1]){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+
+            
+        }
+       
+    }
+}
+
+
+$solution = new Solution();
+$solution->detectCapitalUse("FlaG");
+
+?>
