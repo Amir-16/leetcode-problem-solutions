a4e048a80e508577ecdd3ecc97d5ee94235db3c2
Amir-16 <greenfitter516@gmail.com>
2024-05-03 11:47:57 +0600

    merge with master

---
+++ Easy/205. Isomorphic Strings/solution.php
@@ -0,0 +1,15 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param String $s
+     * @param String $t
+     * @return Boolean
+     */
+    public function isIsomorphic($s, $t)
+    {
+
+    }
+}
---
+++ Medium/165. Compare Version Numbers/solution.php
@@ -0,0 +1,15 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param String $version1
+     * @param String $version2
+     * @return Integer
+     */
+    public function compareVersion($version1, $version2)
+    {
+
+    }
+}
