c756f1482f236c7d31237ea0554be37382316f14
Amir-16 <greenfitter516@gmail.com>
2024-06-16 19:53:34 +0600

    2744

--- Easy/2744. Find Maximum Number of String Pairs/solution.php
+++ Easy/2744. Find Maximum Number of String Pairs/solution.php
@@ -9,9 +9,35 @@ class Solution
     {
         $count = 0;
 
+        $pairedArray = [];
+
         foreach ($words as $word) {
+
+            $revWord = strrev($word);
+
+            if (in_array($revWord, $words) && $revWord !== $word) {
+
+                $pair = [$word, $revWord];
+
+                sort($pair);
+
+                $pairString = implode('', $pair);
+
+                if (!in_array($pairString, $pairedArray)) {
+                    $count++;
+                    $pairedArray[] = $pairString;
+                }
+            }
 
         }
 
+        return $count;
+
     }
 }
+
+$solution = new Solution();
+
+$result = $solution->maximumNumberOfStringPairs(["cd", "ac", "dc", "ca", "zz"]);
+
+print_r($result);
