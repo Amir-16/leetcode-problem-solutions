39547540b38e1320d7a5ad39699bf81babdb43c2
Amir-16 <greenfitter516@gmail.com>
2024-07-06 23:58:44 +0600

    350

--- Easy/350. Intersection of Two Arrays II/solution.php
+++ Easy/350. Intersection of Two Arrays II/solution.php
@@ -1,16 +1,13 @@
 <?php
-class Solution {
-
+class Solution
+{
     /**
      * @param Integer[] $nums1
      * @param Integer[] $nums2
      * @return Integer[]
      */
-
-    //  this interesting prblem check the uniqueness of two integers arrays  . array_intersect first fill
-    // all elements from array1  . thats why we have to check the uniqueness of two integers
-    function intersect(array $nums1, array $nums2) {
-
+    public function intersect(array $nums1, array $nums2)
+    {
         $result = [];
 
         $count1 = array_count_values($nums1);
@@ -18,23 +15,22 @@ class Solution {
 
         foreach ($count1 as $key => $number) {
 
-            if(isset($count2[$key])) {
+            if (isset($count2[$key])) {
 
-                $intersectCount = min($number , $count2[$key]);
+                $intersectCount = min($number, $count2[$key]);
 
-                $result = array_merge($result, array_fill(0 , $intersectCount, $key));
+                $result = array_merge($result, array_fill(0, $intersectCount, $key));
             }
 
         }
-     
+
         return $result;
     }
 }
 
-
 $solution = new Solution();
 
-$nums1 = [1,2,2,1];
+$nums1 = [1, 2, 2, 1];
 
 $nums2 = [2];
 
