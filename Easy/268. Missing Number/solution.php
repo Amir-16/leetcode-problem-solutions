4ce04f2a0e8d8a7c0a32626bcc5cfd51ecb25743
Amir-16 <greenfitter516@gmail.com>
2024-07-14 14:01:06 +0600

    268

--- Easy/268. Missing Number/solution.php
+++ Easy/268. Missing Number/solution.php
@@ -1,13 +1,41 @@
 <?php
 class Solution
 {
-
     /**
      * @param Integer[] $nums
      * @return Integer
      */
     public function missingNumber($nums)
     {
+        $count = count($nums);
+
+        if ($count <= 1) {
+            return $nums[0] == 0? 1 : 0;
+        }
+
+        $max = max($nums);
+
+        $min = min($nums);
+
+        $range = range($min, $max);
+
+        $missingNumber = array_diff($range, $nums);
+
+        if (empty($missingNumber)) {
+
+            return $min == 0? $max + 1 : $min - 1;
+
+        }
+
+        return (int) implode(', ', $missingNumber);
 
     }
 }
+
+$solution = new Solution();
+
+$nums = [1,2];
+
+$solution = $solution->missingNumber($nums); // Output: 2
+
+print_r($solution);
