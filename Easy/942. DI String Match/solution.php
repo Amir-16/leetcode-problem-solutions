e2e7b4c1c9d4660c8c892a09c4683cbd410a54ee
Amir-16 <greenfitter516@gmail.com>
2024-07-15 17:53:49 +0600

    942

--- Easy/942. DI String Match/solution.php
+++ Easy/942. DI String Match/solution.php
@@ -1,11 +1,30 @@
 <?php
-class Solution {
+class Solution
+{
+    public $result;
+    public function diStringMatch($s)
+    {
+        $length = strlen($s);
+        $low = 0;
+        $high = $length;
+        $this->result = [];
 
-    /**
-     * @param String $s
-     * @return Integer[]
-     */
-    function diStringMatch($s) {
-        
+        for ($i = 0; $i < $length; $i++) {
+            if ($s[$i] === 'I') {
+                array_push($this->result, $low++);
+            } else {
+                array_push($this->result, $high--);
+            }
+        }
+
+        $this->result[] = $low;
+
+        return $this->result;
     }
 }
+
+$solution = new Solution();
+
+$result = $solution->diStringMatch('IDID');
+
+print_r($result);
