75e55b1f5508c188a88a9c4956422557dfdbcd06
Amir-16 <greenfitter516@gmail.com>
2024-10-19 19:21:14 +0600

    1971

--- Easy/1971. Find if Path Exists in Graph/solution.php
+++ Easy/1971. Find if Path Exists in Graph/solution.php
@@ -1,6 +1,6 @@
 <?php
-class Solution {
-
+class Solution
+{
     /**
      * @param Integer $n
      * @param Integer[][] $edges
@@ -8,7 +8,62 @@ class Solution {
      * @param Integer $destination
      * @return Boolean
      */
-    function validPath($n, $edges, $source, $destination) {
-        
+    public function validPath($n, $edges, $source, $destination)
+    {
+        $graph = [];
+
+        foreach ($edges as $edge) {
+
+            if (!isset($graph[$edge[0]])) {
+                $graph[$edge[0]] = [];
+            }
+
+            if (!isset($graph[$edge[1]])) {
+                $graph[$edge[1]] = [];
+            }
+            $graph[$edge[0]][] = $edge[1];
+            $graph[$edge[1]][] = $edge[0];
+        }
+
+        $queue = new SplQueue();
+
+        $queue->enqueue($source);
+
+        $visited = array_fill(0, $n, false);
+
+        $visited[$source] = true;
+
+        while (!$queue->isEmpty()) {
+            $node = $queue->dequeue();
+
+            if ($node == $destination) {
+                return true;
+            }
+
+            if (isset($graph[$node])) {
+
+                foreach ($graph[$node] as $neighbour) {
+                    if (!$visited[$neighbour]) {
+                        $visited[$neighbour] = true;
+                        $queue->enqueue($neighbour);
+                    }
+                }
+
+            }
+        }
+
+        return false;
     }
 }
+
+$n = 3;
+
+$edges = [[0, 1], [0, 2], [1, 2]];
+
+$source = 0;
+
+$destination = 2;
+
+$solution = new Solution();
+
+echo $solution->validPath($n, $edges, $source, $destination); // Output: true
