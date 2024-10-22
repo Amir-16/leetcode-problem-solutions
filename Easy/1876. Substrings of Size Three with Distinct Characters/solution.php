1b54b560660dd5ea2db6fde619a7486b3bca58ea
Amir-16 <greenfitter516@gmail.com>
2024-10-22 21:33:20 +0600

    1876

---
+++ Easy/1876. Substrings of Size Three with Distinct Characters/solution.php
@@ -0,0 +1,30 @@
+<?php
+class Solution
+{
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    public function countGoodSubstrings($s)
+    {
+        $count = 0;
+
+        for ($i = 0; $i < strlen($s) - 2; $i++) {
+
+            $subString = substr($s, $i, 3);
+
+            if (count(array_unique(str_split($subString))) === 3) {
+                $count++;
+            }
+        }
+        return $count;
+    }
+}
+
+$s = "xyzzaz";
+
+$solution = new Solution();
+
+$result = $solution->countGoodSubstrings($s);
+
+echo $result;
