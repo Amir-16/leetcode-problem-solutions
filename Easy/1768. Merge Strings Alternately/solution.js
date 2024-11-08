b8700bc6813f54d0126fcd84c1fb5baa39c9c6ca
Amir-16 <greenfitter516@gmail.com>
2024-11-09 00:31:05 +0600

    1768

--- Easy/1768. Merge Strings Alternately/solution.js
+++ Easy/1768. Merge Strings Alternately/solution.js
@@ -1,23 +1,16 @@
 function mergeAlternately(word1, word2) {
-    // To store the final string
+
     let result = "";
-    // For every index in the strings
+
     for (let i = 0; i < word1.length || i < word2.length; i++) {
 
-        // First choose the ith character of the
-        // first string if it exists
-        if (i < word1.length)
-        {
+        if (i < word1.length) {
             result += word1.charAt(i);
-
         }
-        if (i < word2.length)
-        {
+        if (i < word2.length) {
             result += word2.charAt(i);
         }
-        
     }
-
     return result;
 }
 
