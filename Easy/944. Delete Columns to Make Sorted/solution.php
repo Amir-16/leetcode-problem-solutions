d4a30374e47d80145dfbc722cee9461ecc65af90
Amir-16 <greenfitter516@gmail.com>
2024-11-26 00:48:39 +0600

    944

--- Easy/944. Delete Columns to Make Sorted/solution.php
+++ Easy/944. Delete Columns to Make Sorted/solution.php
@@ -1,11 +1,39 @@
 <?php
-class Solution {
-
+class Solution
+{
     /**
      * @param String[] $strs
      * @return Integer
      */
-    function minDeletionSize($strs) {
-        
+    public function minDeletionSize($strs)
+    {
+        $deleteCount = 0;
+
+        $rowCount = count($strs);
+
+        $colCount = strlen($strs[0]);
+
+        for ($col = 0; $col < $colCount; $col++) {
+            $isSorted = true;
+
+            for ($row = 1; $row < $rowCount; $row++) {
+                // Compare only the current column
+                if ($strs[$row][$col] < $strs[$row - 1][$col]) {
+                    $isSorted = false;
+                    break;
+                }
+            }
+
+            if (!$isSorted) {
+                $deleteCount++;
+            }
+        }
+        return $deleteCount;
     }
 }
+
+$solution = new Solution();
+
+$strs = ["cba", "daf", "ghi"];
+
+echo $solution->minDeletionSize($strs); // Output: 1
