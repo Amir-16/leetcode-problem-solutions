f1c3d64014a18bb4577def47e26a4d4809abf8b9
Amir-16 <greenfitter516@gmail.com>
2025-01-28 00:21:25 +0600

    2053

---
+++ Easy/2053. Kth Distinct String in an Array/solution.php
@@ -0,0 +1,34 @@
+<?php
+class Solution
+{
+    /**
+     * @param String[] $arr
+     * @param Integer $k
+     * @return String
+     */
+    public function kthDistinct($arr, $k)
+    {
+        $counts = array_count_values($arr);
+
+        $result = array_filter($arr, callback: function ($value) use ($counts) {
+            return $counts[$value] === 1;
+        });
+
+        $arrValues = array_values($result);
+
+        if ($k > count(value: $arrValues)) {
+            return "";
+        }
+
+        return $arrValues[$k - 1];
+    }
+}
+
+$arr = ["d", "b", "c", "b", "c", "a"];
+
+$k = 2;
+
+$solution = new Solution();
+$result   = $solution->kthDistinct($arr, $k);
+
+print_r($result);
