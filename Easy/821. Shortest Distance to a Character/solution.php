9f18ab87a5d4649e10454d7ec2135e8cf43554d6
Amir-16 <greenfitter516@gmail.com>
2025-03-04 01:10:51 +0600

    821

--- Easy/821. Shortest Distance to a Character/solution.php
+++ Easy/821. Shortest Distance to a Character/solution.php
@@ -1,12 +1,40 @@
 <?php
-class Solution {
-
+class Solution
+{
     /**
      * @param String $s
      * @param String $c
      * @return Integer[]
      */
-    function shortestToChar($s, $c) {
-        
+    public function shortestToChar($s, $c)
+    {
+        $length = strlen($s);
+
+        $result = [];
+
+        $pos = -$length;
+        for ($i = 0; $i < $length; $i++) {
+            if ($s[$i] === $c) {
+                $pos = $i;
+            }
+            $result[$i] = $i - $pos;
+        }
+
+        $pos = 2 * $length;
+
+        for ($i = $length - 1; $i >= 0; $i--) {
+            if ($s[$i] === $c) {
+                $pos = $i;
+            }
+            $result[$i] = min($result[$i], abs($pos - $i));
+        }
+
+        return $result;
     }
 }
+
+$solution = new Solution();
+$s        = "loveleetcode";
+$c        = "e";
+$output   = $solution->shortestToChar($s, $c);
+print_r($output); // Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
