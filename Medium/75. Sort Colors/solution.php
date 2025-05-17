d1fe33ae5daa0a3fd90dded0c087b1f1bb73ea48
Amir-16 <greenfitter516@gmail.com>
2025-05-17 20:00:42 +0600

    75

--- Medium/75. Sort Colors/solution.php
+++ Medium/75. Sort Colors/solution.php
@@ -7,6 +7,18 @@ class Solution
      */
     public function sortColors(&$nums)
     {
+        sort($nums);
 
+        return $nums;
     }
 }
+
+$solution = new Solution();
+
+$nums = [2, 0, 2, 1, 1, 0];
+
+$result = $solution->sortColors($nums);
+
+print_r($result);
+
+
