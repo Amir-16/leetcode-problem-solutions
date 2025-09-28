cb910d0265670452d7602e05d4e7ee552f1a7232
Amir-16 <greenfitter516@gmail.com>
2025-09-29 01:26:07 +0600

    976

--- Easy/976. Largest Perimeter Triangle/solution.php
+++ Easy/976. Largest Perimeter Triangle/solution.php
@@ -1,27 +1,24 @@
 <?php
 class Solution
 {
-
-   
-   // public $nums;
-    function largestPerimeter(array $nums)
+    public function largestPerimeter(array $nums)
     {
         rsort($nums);
+
         $length = count($nums);
+
         for ($i = 0; $i < $length - 2; $i++) {
             if (($nums[$i]   + $nums[$i + 1] > $nums[$i + 2])
                 && ($nums[$i]   + $nums[$i + 2] > $nums[$i + 1])
                 && ($nums[$i + 1] + $nums[$i + 2] > $nums[$i])
             ) {
-                echo $nums[$i] + $nums[$i + 1] + $nums[$i + 2];
+                return $nums[$i] + $nums[$i + 1] + $nums[$i + 2];
             }
         }
-        echo 0;
+        return 0;
     }
 }
 
 $solution = new Solution;
+
 $solution->largestPerimeter([3, 6, 2, 3]);
-
-
-?>
