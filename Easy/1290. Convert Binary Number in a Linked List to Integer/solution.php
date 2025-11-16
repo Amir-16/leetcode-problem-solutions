185282f9fe7370383cc96a509525e8a22314a7da
Amir-16 <greenfitter516@gmail.com>
2025-11-17 00:18:02 +0600

    1290

---
+++ Easy/1290. Convert Binary Number in a Linked List to Integer/solution.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution
+{
+    /**
+     * @param ListNode $head
+     * @return Integer
+     */
+    public function getDecimalValue($head)
+    {
+        $num = 0;
+        while ($head !== null) {
+            $num  = ($num << 1) | $head->val;
+            $head = $head->next;
+        }
+        return $num;
+    }
+}
